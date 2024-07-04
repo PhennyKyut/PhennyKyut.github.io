77a1c9a381df2faff0e8db28e9069892383c8267
PhennyKyut <jcbuensalida21@gmail.com>
2024-07-04 23:42:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ var product2 = document.getElementById("product2")
 var qty2 = document.getElementById("qty2")
 var price2 = document.getElementById("price2")
 
+var product3 = document.getElementById("product3")
+var qty3 = document.getElementById("qty3")
+var price3 = document.getElementById("price3")
+
+var product4 = document.getElementById("product4")
+var qty4 = document.getElementById("qty4")
+var price4 = document.getElementById("price4")
+
+var product5 = document.getElementById("product5")
+var qty5 = document.getElementById("qty5")
+var price5 = document.getElementById("price5")
+
+var product6 = document.getElementById("product6")
+var qty6 = document.getElementById("qty6")
+var price6 = document.getElementById("price6")
+
 var carts = document.getElementById("carts")
 var total = document.getElementById("total")
 var cash = document.getElementById("cash")
@@ -24,7 +40,31 @@ function addOrder(){
         //carts.textContent += carts.value.toString() + "\n";
         carts.textContent += order
     }
+    if (parseFloat(qty3.value) > 0){
+        var order= qty3.value.toString() + ' pc/s x '+ price3.textContent + '------'+ product3.textContent + '------ Php' + (parseFloat(qty3.value)*parseFloat(price3.textContent)) + '\n'
+        //carts.textContent += carts.value.toString() + "\n";
+        carts.textContent += order
+    }
+    if (parseFloat(qty4.value) > 0){
+        var order= qty4.value.toString() + ' pc/s x '+ price4.textContent + '------'+ product4.textContent + '------ Php' + (parseFloat(qty4.value)*parseFloat(pric4.textContent)) + '\n'
+        //carts.textContent += carts.value.toString() + "\n";
+        carts.textContent += order
+    }
+    if (parseFloat(qty5.value) > 0){
+        var order= qty5.value.toString() + ' pc/s x '+ price5.textContent + '------'+ product5.textContent + '------ Php' + (parseFloat(qty5.value)*parseFloat(price5.textContent)) + '\n'
+        //carts.textContent += carts.value.toString() + "\n";
+        carts.textContent += order
+    }
+    if (parseFloat(qty6.value) > 0){
+        var order= qty6.value.toString() + ' pc/s x '+ price6.textContent + '------'+ product6.textContent + '------ Php' + (parseFloat(qty6.value)*parseFloat(price6.textContent)) + '\n'
+        //carts.textContent += carts.value.toString() + "\n";
+        carts.textContent += order
+    }
 }
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
+qty3.addEventListener("keyup", addOrder);
+qty4.addEventListener("keyup", addOrder);
+qty5.addEventListener("keyup", addOrder);
+qty6.addEventListener("keyup", addOrder);
 
